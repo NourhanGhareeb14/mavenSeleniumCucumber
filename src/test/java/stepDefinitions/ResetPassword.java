0aac578211df9ac9a05264346e4a4b63ca62de6e
nourhan ghareeb <n.ghareeb@qpix.io>
2022-03-21 15:49:02 +0200

    add  POM for reset password  screen

--- src/test/java/stepDefinitions/ResetPassword.java
+++ src/test/java/stepDefinitions/ResetPassword.java
@@ -1,44 +1,47 @@
 package stepDefinitions;
 
 import Pages.ResetPasswordPage;
+import Pages.loginPage;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
 import org.testng.Assert;
 
 public class ResetPassword {
-   // ResetPasswordPage resetPasswordPage;
-   @FindBy(className = "content")
-   WebElement message;
-    @Given("user go to login and press into forget password")
-    public void user_GoTologinAndPressResetPassword()  {
+     ResetPasswordPage resetPasswordPage;
+      @Given("user go to login and press into forget password")
+    public void user_GoTologinAndPressResetPassword() throws InterruptedException {
+           resetPasswordPage=new ResetPasswordPage(Hooks.driver);
         Hooks.driver.navigate().to("https://demo.nopcommerce.com/login?returnUrl=%2F");
 
-        Hooks.driver.findElement(By.className("forgot-password")).click();
+      //  Hooks.driver.findElement(By.className("forgot-password")).click();
+          resetPasswordPage.click_forget();
 
     }
     @When("user add email address  and click recover")
-    public void UserAddEmailAndRecover() throws InterruptedException {
-        Thread.sleep(3000);
-        Hooks.driver.findElement(By.className("email")).sendKeys("n.ghareeb@qpix.io");
-          //resetPasswordPage.addMail("n.ghareeb@qpix.io");
-        //buttons
-        Hooks.driver.findElement(By.className("buttons")).click();
-        Thread.sleep(3000);
+    public void UserAddEmailAndRecover()
+    {
+        resetPasswordPage=new ResetPasswordPage(Hooks.driver);
+            resetPasswordPage.enter_email("n.ghareeb@qpix.io");
+        //Hooks.driver.findElement(By.className("email")).sendKeys("n.ghareeb@qpix.io");
 
-
+       // Hooks.driver.findElement(By.name("send-email")).click();
+        resetPasswordPage.click_recover();
     }
 
     @Then("user get alert of mail sent to your mail")
     public void user_getAlert() throws InterruptedException {
 
         String expecetedResult = "Email with instructions has been sent to you.";
-     //   String actualResult =Hooks.driver.findElement(By.className("content")).getText();
-     //   Assert.assertEquals(actualResult.contains(expecetedResult),true);
+        Thread.sleep(3000);
+        String actualResult =Hooks.driver.findElement(By.className("content")).getText();
+        System.out.println(actualResult);
+         Assert.assertEquals(actualResult.contains(expecetedResult),true);
+
+
+
     }
 }
